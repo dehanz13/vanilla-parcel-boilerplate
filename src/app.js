2e110da22cb6f5ce038c518880bfcff365671d4d
dehanz13 <hansel.danniel@gmail.com>
2021-09-05 06:51:12 -0500

    added main.js to load app but still in progress

--- src/app.js
+++ src/app.js
@@ -8,11 +8,21 @@ import "./scss/app.scss";
 
 console.log("Feel free to use this boilerplate!");
 
-const app = async () => {
+const App = async () => {
+  // const template = document.createElement("template");
+  // template.innerHTML = `
+  // 		<div class="container">
+  // 			${Header()}
+  // 			${await User()}
+  // 		</div>
+  // 	`;
+  // // Return a new node from template
+  // return template.content.cloneNode(true);
+
   document.getElementById("navbar-content").innerHTML = Navbar();
   document.getElementById("hero").innerHTML = Hero();
   // document.getElementById("user").innerHTML = await User();
 };
 
-// Init app
-app();
+// Init App
+App();
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import App from "./App";
+
+const app = async () => {
+  document.getElementById("app").appendChild(await App());
+};
+// Load app
+app();
